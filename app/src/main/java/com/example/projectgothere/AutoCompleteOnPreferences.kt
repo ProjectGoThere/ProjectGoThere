873f155b9a9b514bf797d085dbbf82b057f7780a
maritzabeltran <maritzabeltran@gustavus.edu>
2023-04-28 12:21:26 -0500

    updated layout

--- app/src/main/java/com/example/projectgothere/AutoCompleteOnPreferences.kt
+++ app/src/main/java/com/example/projectgothere/AutoCompleteOnPreferences.kt
@@ -71,7 +71,7 @@ class AutoCompleteOnPreferences : AppCompatAutoCompleteTextView {
                 val prefArray = JSONArray(prefString)
                 val result = arrayListOf<String>()
                 for (i in 0 until prefArray.length()) {
-                    result.add(prefArray.getString(i))
+                    result[i] = prefArray.getString(i)
                 }
                 result
             } catch (e: JSONException) {
