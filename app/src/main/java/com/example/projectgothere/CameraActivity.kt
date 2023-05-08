8455116bc6462b486ce23120e4784d910f896416
maritzabeltran <maritzabeltran@gustavus.edu>
2023-05-07 20:01:29 -0500

    camera acttivity connected to dialog box, and camera and gallery buttons are clickable

--- app/src/main/java/com/example/projectgothere/CameraActivity.kt
+++ app/src/main/java/com/example/projectgothere/CameraActivity.kt
@@ -4,11 +4,16 @@ import android.Manifest
 import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.icu.text.DateFormat.getDateTimeInstance
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import android.view.View
+import android.webkit.MimeTypeMap
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
@@ -20,6 +25,8 @@ import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.projectgothere.databinding.ActivityCameraBinding
+import java.io.File
+import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.ExecutorService
@@ -30,6 +37,11 @@ typealias LumaListener = (luma: Double) -> Unit
 
 
 class CameraActivity : AppCompatActivity() {
+    val CAMERA_PERM_CODE = 101
+    val CAMERA_REQUEST_CODE = 102
+    val GALLERY_REQUEST_CODE = 105
+    private lateinit var selectedImage: ImageView
+    private lateinit var currentPhotoPath: String
     private lateinit var viewBinding: ActivityCameraBinding
     private var imageCapture: ImageCapture? = null
     private lateinit var cameraExecutor: ExecutorService
@@ -58,6 +70,52 @@ class CameraActivity : AppCompatActivity() {
             }
         }
         cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    private fun askCameraPermissions() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                CAMERA_PERM_CODE
+            )
+        } else {
+            takePhoto()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                val f = File(currentPhotoPath)
+                selectedImage.setImageURI(Uri.fromFile(f))
+                Log.d("tag", "Absolute Url of Image is " + Uri.fromFile(f))
+                val mediaScanIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
+                val contentUri: Uri = Uri.fromFile(f)
+                mediaScanIntent.data = contentUri
+                this.sendBroadcast(mediaScanIntent)
+            }
+        }
+        if (requestCode == GALLERY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                val contentUri: Uri? = data!!.data
+                val timeStamp = getDateTimeInstance().format(Date())
+                val imageFileName = "JPEG_" + timeStamp + "." + contentUri?.let { getFileExt(it) }
+                Log.d("tag", "onActivityResult: Gallery Image Uri:  $imageFileName")
+                selectedImage.setImageURI(contentUri)
+            }
+        }
+    }
+
+    private fun getFileExt(contentUri: Uri): String? {
+        val c = contentResolver
+        val mime = MimeTypeMap.getSingleton()
+        return mime.getExtensionFromMimeType(c.getType(contentUri))
     }
 
     private fun takePhoto() {
@@ -137,6 +195,25 @@ class CameraActivity : AppCompatActivity() {
             }, ContextCompat.getMainExecutor(this))
         }
 
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File? {
+        // Create an image file name
+        val timeStamp = getDateTimeInstance().format(Date())
+        val imageFileName = "JPEG_" + timeStamp + "_"
+        //        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        val storageDir: File =
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+        val image = File.createTempFile(
+            imageFileName,  /* prefix */
+            ".jpg",  /* suffix */
+            storageDir /* directory */
+        )
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.absolutePath
+        return image
+    }
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
             baseContext, it) == PackageManager.PERMISSION_GRANTED
@@ -145,20 +222,6 @@ class CameraActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         cameraExecutor.shutdown()
-    }
-
-    companion object {
-        private const val TAG = "CameraXApp"
-        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
-        private const val REQUEST_CODE_PERMISSIONS = 10
-        private val REQUIRED_PERMISSIONS =
-            mutableListOf (
-                Manifest.permission.CAMERA
-            ).apply {
-                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
-                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                }
-            }.toTypedArray()
     }
 
     override fun onRequestPermissionsResult(
@@ -175,5 +238,18 @@ class CameraActivity : AppCompatActivity() {
                 finish()
             }
         }
+    }
+    companion object {
+        private const val TAG = "CameraXApp"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf (
+                Manifest.permission.CAMERA
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
     }
 }
