f728011ba962cf9eaa5168472b80036239a3894e
choernem <choernem@gustavus.edu>
2023-05-09 13:01:50 -0500

    rebase onto master

--- app/src/main/java/com/example/projectgothere/CameraActivity.kt
+++ app/src/main/java/com/example/projectgothere/CameraActivity.kt
@@ -2,31 +2,17 @@ package com.example.projectgothere
 
 import android.Manifest
 import android.content.ContentValues
-import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import android.net.Uri
-import android.icu.text.DateFormat.getDateTimeInstance
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
-import android.util.Log
-import android.view.View
-import android.webkit.MimeTypeMap
-import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.PreviewView
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import androidx.core.view.drawToBitmap
@@ -37,14 +23,6 @@ import java.io.File
 import java.io.OutputStream
 import java.text.DateFormat.getDateTimeInstance
 import java.util.*
-import androidx.core.content.FileProvider
-import com.example.projectgothere.databinding.ActivityCameraBinding
-import java.io.File
-import java.io.IOException
-import java.text.SimpleDateFormat
-import java.util.*
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 
 
 
